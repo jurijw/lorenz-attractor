ad0a6b41fa2bae47aed5f493e4233eeef8fac95a
Jurij Waite <jurijlwaite@gmail.com>
2024-08-02 13:06:49 -0700

    Added support for drawing multiple lines.

--- lorenz.js
+++ lorenz.js
@@ -13,91 +13,125 @@ camera.position.set(0, 0, 100);
 
 // Configure camera controls
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.screenSpacePanning = true;
-controls.maxPolarAngle = Math.PI / 2;
-// controls.autoRotate = true;
-// controls.autoRotateSpeed = 10000;
+controls.maxPolarAngle = 2 * Math.PI; // TODO: Does this even do anything?
+controls.autoRotate = true;
+controls.autoRotateSpeed = 1;
 
 // Setup the scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
-// Configure the line and leading point materials
-const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
-const pointMaterial = new THREE.PointsMaterial({
-  color: 0x3300ff,
-  size: 5,
-  sizeAttenuation: false,
-})
-
 // Constants for the attractor
-const rho = 28;
-const sigma = 10;
-const beta = 8 / 3;
+const RHO = 28;
+const SIGMA = 10;
+const BETA = 8 / 3;
 // Constants for the simulation
-const dt = 0.001;
-const maxPoints = 30000; // Maximum number of points in the attractor line
+const NUMLINES = 3;
+const MAXPOINTSPERLINE = 60000; // Maximum number of points in the attractor line
+const dt = 0.01 // Timestep
+
+const LINECOLORS = [0xff0000, 0x00ff00, 0x0000ff];
+
+const LINEMATERIALS = [];
+const POINTMATERIALS = [];
+for (let i = 0; i < NUMLINES; i++) {
+  // Configure the line and leading point materials
+  LINEMATERIALS.push(new THREE.LineBasicMaterial({ color: LINECOLORS[i] }));
+  POINTMATERIALS.push(new THREE.PointsMaterial({
+    color: LINECOLORS[(i + 1) % LINECOLORS.length],
+    size: 5,
+    sizeAttenuation: false,
+  }));
+}
+
 
 const delta = function (pos, dt) {
   // Given a position vector, calculate the change in that vector 
   // according to the Lorenz differential equations
-  const dx = sigma * (pos.y - pos.x);
-  const dy = pos.x * (rho - pos.z) - pos.y;
-  const dz = pos.x * pos.y - beta * pos.z;
-  return new THREE.Vector3(dx, dy, dz);
+  const dx = SIGMA * (pos.y - pos.x);
+  const dy = pos.x * (RHO - pos.z) - pos.y;
+  const dz = pos.x * pos.y - BETA * pos.z;
+  return new THREE.Vector3(dx, dy, dz).multiplyScalar(dt);
 };
 
-const newPos = function (pos) {
+const newPos = function (pos, dt) {
   // Given a position vector, return an updated one according to the
   // Lorenz differential equations
-  const dp = delta(pos).multiplyScalar(dt);
+  const dp = delta(pos, dt);
   return pos.clone().add(dp);
 };
 
-const pos0 = new THREE.Vector3(0.01, 0, 0); // Initial condition
-// Prefill the point array, so that the bufferGeometry has the correct size
-// at instantiation (resizing is costly)
+// Initial displacement vectors.
+const r0s = [
+  new THREE.Vector3(0.01, 0, 0),
+  new THREE.Vector3(0, 5, 5),
+  new THREE.Vector3(0.0, 5, 0),
+];
 
-let points = [];
-for (let i = 0; i < maxPoints; i++) {
-  points.push(pos0);
+const ALLPOINTS = [];
+const ALLLINEGEOMETRIES = [];
+const ALLLINES = [];
+for (let i = 0; i < NUMLINES; i++) {
+  let r0_i = r0s[i]; // Initial condition
+  // Prefill the point array, so that the bufferGeometry has the correct size
+  // at instantiation (resizing is costly)
+
+  let points_i = [];
+  for (let j = 0; j < MAXPOINTSPERLINE; j++) {
+    points_i.push(r0_i);
+  }
+  ALLPOINTS.push(points_i);
+
+  const lineGeometry_i = new THREE.BufferGeometry().setFromPoints(points_i);
+  const lineMaterial_i = LINEMATERIALS[i];
+  const line_i = new THREE.Line(lineGeometry_i, lineMaterial_i);
+  scene.add(line_i);
+  ALLLINEGEOMETRIES.push(lineGeometry_i);
+  ALLLINES.push(line_i);
 }
-// let points = [pos0, newPos(pos0)];
 
-const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(lineGeometry, lineMaterial);
-scene.add(line);
 
-// Add the leading point as a colored one
-let pointPosition = points[points.length - 1];
-const pointGeometry = new THREE.BufferGeometry();
-pointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(pointPosition.toArray(), 3));
-const point = new THREE.Points(pointGeometry, pointMaterial);
-scene.add(point);
+//// Add the leading point as a colored one
+//let pointPosition = points[points.length - 1];
+//const pointGeometry = new THREE.BufferGeometry();
+//pointGeometry.setAttribute('position', new THREE.Float32BufferAttribute(pointPosition.toArray(), 3));
+//const point = new THREE.Points(pointGeometry, pointMaterial);
+//scene.add(point);
 
-let numRepeats = 10;
+let numRepeats = 1;
 let lastTime = 0; // ms
 const animate = function (time) {
   if (time - lastTime > 1) {
     for (let i = 0; i < numRepeats; i++) {
-      const current_pos = points[points.length - 1];
-      points.push(newPos(current_pos));
-      if (points.length > maxPoints) {
-        points.shift(); // Remove the oldest point
+      for (let lineIndex = 0; lineIndex < NUMLINES; lineIndex++) {
+        // Get data for a specific line
+        const points = ALLPOINTS[lineIndex];
+        const lineGeometry = ALLLINEGEOMETRIES[lineIndex];
+
+        const current_pos = points[points.length - 1];
+        points.push(newPos(current_pos, dt));
+        if (points.length > MAXPOINTSPERLINE) { // TODO: Can we get rid of if since we are prepopulating arrays?
+          points.shift(); // Remove the oldest point
+        }
+
+        // Update geometry with new points
+        lineGeometry.setFromPoints(points);
+
+        // // Update leading point
+        // pointGeometry.attributes.position.set(points[points.length - 1].toArray());
+        // pointGeometry.attributes.position.needsUpdate = true;
+
+        lastTime = time;
       }
-
-      // Update geometry with new points
-      lineGeometry.setFromPoints(points);
-
-      // Update leading point
-      pointGeometry.attributes.position.set(points[points.length - 1].toArray());
-      pointGeometry.attributes.position.needsUpdate = true;
-
-      lastTime = time;
     }
   }
+
+  // Update camera controls
+  controls.update();
 
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
@@ -105,8 +139,6 @@ const animate = function (time) {
 
 requestAnimationFrame(animate);
 
-// Update camera controls
-controls.update();
 
 // Resize correctly when window size changes
 window.addEventListener('resize', () => {
